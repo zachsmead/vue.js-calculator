3a6e688d76cbad22f44875c816967b1392f1c955
= <=>
2017-04-30 15:08:24 -0700

    added String conversions to vue calculator

--- calculator.js
+++ calculator.js
@@ -11,16 +11,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			enterSymbol: function(symbol) {
 				// if the current input symbol is a number 
 				if (Number.isInteger(symbol)) {
-					// // if the last input symbol was a number
-					// if (this.numbers.includes(this.expression[this.expression.length-1])) {
-					// 	// add it to the one's place of that number
-					// 	var index = this.expression.length - 1;
-					// 	var current_num = this.expression[index] * 10;
-					// 	this.expression.splice(index = current_num + symbol;
-					// } else {
-					// 	this.expression.push(symbol);
-					// }
-					// console.log(this.expression);
 					if (this.expression == '0') {
 						this.expression = String(symbol);
 					} else {
