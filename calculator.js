dffeb25d96814feb86a0c9c84cf8d2125055628c
= <=>
2017-05-03 23:34:45 -0700

    Formatting.

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var app = new Vue({
 		el: '#app',
 		data: {
-			numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+			buttons: ["C", 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "-", "x", "/", "="],
 			operators: ["+", "-", "*", "/"],
 			expression: '0',
 			answer: 0
@@ -20,15 +20,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				// if the current input symbol is an operator and the last input was not an operator
 				} else if (this.operators.includes(symbol) 
 						&& this.expression[this.expression.length - 1] != symbol 
-						&& this.expression.length > 0) 
+						&& this.expression.length > 0) // and you aren't starting the expression with an operator
 				{
 					this.expression += symbol;
-					console.log(this.expression);
+				} else if (symbol == "=") {
+					return this.evaluate();
+				} else if (symbol == "C") {
+					this.expression = '0';
 				}
 			},
 
 			evaluate: function() {
-				this.answer = eval(this.expression);
+				this.expression = eval(this.expression);
 			}
 		}
 	});
